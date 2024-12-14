793dfc1510a550ffd50c38200c789782eb1b4062
Sourav Dev <130246603+souravdev33@users.noreply.github.com>
2024-12-15 00:53:20 +0600

    committed

--- cwh python/chapter 4/list.py
+++ cwh python/chapter 4/list.py
@@ -1,4 +1,7 @@
-li=[5,7,2,5,6,9]
-li.sort()
-li.append(6)
-print(li)
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes" # Unlike Strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
